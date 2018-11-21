e09be3f873f68e262e65bd6e3278dad667a42b4c
nimbus117 <mail@nimbus117.co.uk>
2018-11-21 23:31:01 +0000

    added home/about pages

---
+++ application/views/pages/about.php
@@ -0,0 +1,1 @@
+About - Ipsum enim non ducimus molestias recusandae. Voluptatum sint blanditiis odit eos voluptas. Nisi fugiat exercitationem non tenetur neque Odio fugiat iste molestias blanditiis dolorem? Obcaecati esse ex autem provident vel!
---
+++ application/views/pages/home.php
@@ -0,0 +1,2 @@
+Home - Ipsum enim non ducimus molestias recusandae. Voluptatum sint blanditiis odit eos voluptas. Nisi fugiat exercitationem non tenetur neque Odio fugiat iste molestias blanditiis dolorem? Obcaecati esse ex autem provident vel!
+
