dd47e6f8657d88183f7d6641abf8703833711c87
nimbus117 <mail@nimbus117.co.uk>
2018-11-22 00:08:45 +0000

    renamed pages.php to Pages.php and added controller logic

--- application/views/pages/about.php
+++ application/views/pages/about.php
@@ -1,1 +1,1 @@
-About - Ipsum enim non ducimus molestias recusandae. Voluptatum sint blanditiis odit eos voluptas. Nisi fugiat exercitationem non tenetur neque Odio fugiat iste molestias blanditiis dolorem? Obcaecati esse ex autem provident vel!
+Ipsum enim non ducimus molestias recusandae. Voluptatum sint blanditiis odit eos voluptas. Nisi fugiat exercitationem non tenetur neque Odio fugiat iste molestias blanditiis dolorem? Obcaecati esse ex autem provident vel!
--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,2 +1,1 @@
-Home - Ipsum enim non ducimus molestias recusandae. Voluptatum sint blanditiis odit eos voluptas. Nisi fugiat exercitationem non tenetur neque Odio fugiat iste molestias blanditiis dolorem? Obcaecati esse ex autem provident vel!
-
+Ipsum enim non ducimus molestias recusandae. Voluptatum sint blanditiis odit eos voluptas. Nisi fugiat exercitationem non tenetur neque Odio fugiat iste molestias blanditiis dolorem? Obcaecati esse ex autem provident vel!
